a3b6ecaedb8c2a90159ec3088914f53e22b3e13d
Marcelo Vonkunoschy <marcelo.vonku@xnlatam.com>
2025-04-13 10:17:34 -0300

    agregamos header con menú

---
+++ include/header.php
@@ -0,0 +1,22 @@
+<!-- @format html -->
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf8">
+    <meta name="view-port" content="width=divise-width, initial-scale=1.0">
+    <title>Portafolio | Marcelo Vonkunoschy</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+</head>
+
+<body>
+    <nav class="site-header">
+        <div class="logo">MV</div>
+        <ul class="nav-link">
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="about.php">Sobre mi</a></li>
+            <li><a href="proyectos.php">Proyectos</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+        </ul>
+    </nav>
+</body>
