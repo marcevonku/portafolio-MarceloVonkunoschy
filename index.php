b061fc489182b3a2ad03063582606bc98a29f71e
Marcelo Vonkunoschy <marcelo.vonku@xnlatam.com>
2025-04-13 09:36:26 -0300

    estructura básica en el archivo index

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include 'include/header.php'; ?>
+
+<main class="container">
+    <section class="hero">
+        <h1>Marcelo Milton Vonkunoschy</h1>
+        <h2>Desarrollador PHP</h2>
+        <P>Bienvenido a mi portafolio. Aquí encontraras información sobre mi, mis proyectos y como contactarme.</P>
+        <a href="proyectos.php" class="btn">Ver proyectos</a>
+    </section>
+</main>
+
+<?php include 'include/footer.php'; ?>
