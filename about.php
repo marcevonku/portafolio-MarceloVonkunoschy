0024eb20d110dfa0f9afda43b98612826bf268ff
Marcelo Vonkunoschy <marcelo.vonku@xnlatam.com>
2025-04-17 17:37:28 -0300

    bajamos brillo a imagenes de index y about

--- about.php
+++ about.php
@@ -1,34 +1,26 @@
 <!-- about.php -->
 
 <?php include 'include/header.php'; ?>
-
-<main class="container about-section">
-    <div class="about-content">
-        <section class="hero">
-            <h1>Sobre mi</h1>
-            <h2>Desarrollador PHP Junior | Apacionado por la tecnología</h2>
-            <p>Analista y programador superior, especializado en php.</p>
-            <p>
-                Programador especializado en PHP y framework Yii2 Advanced. &nbsp Poseo conocimientos en administracion de bases de datos MySQL y PostgreSQL.<br>
-                Actualmente me desempeño como backend en <a href="https://www.xnativa.com/" style="color: #6dd8ff">XNet</a>
-                Tengo experiencia trabajando con metodologías ágiles como SCRUM y comprendo cada etapa del flujo de trabajo en un proyecto.<br>
-                Por otra parte, manejo las herramientas necesarias para integrarme a proyectos mediante repositorios como GitHub o GitLab.<br>
-                Me caracteriza una personalidad proactiva, me apasiona resolver problemas, escribir código limpio y seguir aprendiendo día a día.
-                .
-            </p>
-            <p>
-                Trabajo principalmente con HTML, CSS, PHP, librerías como bootstrap. &nbsp; Para proyectos de mayor envergadura, utilizo frameworks como Yii2.<br>
-                Actualmente estoy desarrollando mi portafolio personal como una forma de mostrar mis proyectos y evolución como desarrollador.<br>
-            </p>
-            <p>
-                Enfocado en mejorar continua.
-            </p>
-
-        </section>
+<main class="container">
+    <section class="hero">
+        <h1>Sobre mi</h1>
+        <h2>Desarrollador PHP Junior | Apacionado por la tecnología</h2>
+        <p>Analista y programador superior, especializado en php.</p>
+        <p>
+            Programador especializado en PHP y framework Yii2 Advanced. &nbsp Poseo conocimientos en administracion de bases de datos MySQL y PostgreSQL.<br>
+            Actualmente me desempeño como backend en <a href="https://www.xnativa.com/" style="color: #6dd8ff">XNet</a>
+            Tengo experiencia trabajando con metodologías ágiles como SCRUM y comprendo cada etapa del flujo de trabajo en un proyecto.<br>
+            Por otra parte, manejo las herramientas necesarias para integrarme a proyectos mediante repositorios como GitHub o GitLab.<br>
+            Me caracteriza una personalidad proactiva, me apasiona resolver problemas, escribir código limpio y seguir aprendiendo día a día.
+            .
+        </p>
+        <p>
+            Trabajo principalmente con HTML, CSS, PHP, librerías como bootstrap. &nbsp; Para proyectos de mayor envergadura, utilizo frameworks como Yii2.<br>
+            Actualmente estoy desarrollando mi portafolio personal como una forma de mostrar mis proyectos y evolución como desarrollador.<br>
+        </p>
+        <p>
+            Enfocado en mejorar continua.
+        </p>
+    </section>
 </main>
-
 <?php include 'include/footer.php'; ?>
-
-</section>
-
-</main>
