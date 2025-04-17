09701c5412425125b8cd6bc78bf9ce0fdc4a2e10
Marcelo Vonkunoschy <marcelo.vonku@xnlatam.com>
2025-04-17 17:55:12 -0300

    archivo about mejorando responcividad

--- about.php
+++ about.php
@@ -5,20 +5,14 @@
     <section class="hero">
         <h1>Sobre mi</h1>
         <h2>Desarrollador PHP Junior | Apacionado por la tecnología</h2>
-        <p>Analista y programador superior, especializado en php.</p>
-        <p>
+        <p>Analista y programador superior, especializado en php.<br>
             Programador especializado en PHP y framework Yii2 Advanced. &nbsp Poseo conocimientos en administracion de bases de datos MySQL y PostgreSQL.<br>
             Actualmente me desempeño como backend en <a href="https://www.xnativa.com/" style="color: #6dd8ff">XNet</a>
             Tengo experiencia trabajando con metodologías ágiles como SCRUM y comprendo cada etapa del flujo de trabajo en un proyecto.<br>
             Por otra parte, manejo las herramientas necesarias para integrarme a proyectos mediante repositorios como GitHub o GitLab.<br>
-            Me caracteriza una personalidad proactiva, me apasiona resolver problemas, escribir código limpio y seguir aprendiendo día a día.
-            .
-        </p>
-        <p>
+            Me caracteriza una personalidad proactiva, me apasiona resolver problemas, escribir código limpio y seguir aprendiendo día a día.<br>
             Trabajo principalmente con HTML, CSS, PHP, librerías como bootstrap. &nbsp; Para proyectos de mayor envergadura, utilizo frameworks como Yii2.<br>
             Actualmente estoy desarrollando mi portafolio personal como una forma de mostrar mis proyectos y evolución como desarrollador.<br>
-        </p>
-        <p>
             Enfocado en mejorar continua.
         </p>
     </section>
