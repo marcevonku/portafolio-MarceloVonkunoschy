20f932c3dac5127f23585f16391584b8909d4244
Marcelo Vonkunoschy <marcelo.vonku@xnlatam.com>
2025-04-17 13:22:11 -0300

    trabajando en <p> section about

--- about.php
+++ about.php
@@ -7,23 +7,23 @@
         <section class="hero">
             <h1>Sobre mi</h1>
             <h2>Desarrollador PHP Junior | Apacionado por la tecnología</h2>
-            <p>Soy Marcelo Vonkunoschy, analista y programador superior, especializado en php.</p>
+            <p>Analista y programador superior, especializado en php.</p>
             <p>
-                Programador junior especializado en PHP y el framework Yii2 Advanced.
-                Poseo conocimientos en bases de datos MySQL y PostgreSQL.
-                Actualmente me desempeño como backend en Xnet.
-                Tengo experiencia trabajando con metodologías ágiles como SCRUM y comprendo cada etapa del flujo de trabajo en un proyecto.
-                Además, manejo las herramientas necesarias para integrarme a proyectos mediante repositorios como GitHub o GitLab.
-                Me apasiona resolver problemas, escribir código limpio y seguir aprendiendo día a día.
-                Me caracteriza una personalidad proactiva.
+                Programador especializado en PHP y framework Yii2 Advanced.<br>
+                Poseo conocimientos en administracion de bases de datos MySQL y PostgreSQL.<br>
+                Actualmente me desempeño como backend en <a href="https://www.xnativa.com/" style="color: #6dd8ff">XNet</a>
+                Tengo experiencia trabajando con metodologías ágiles como SCRUM y comprendo cada etapa del flujo de trabajo en un proyecto.<br>
+                Por otra parte, manejo las herramientas necesarias para integrarme a proyectos mediante repositorios como GitHub o GitLab.<br>
+                Me caracteriza una personalidad proactiva, me apasiona resolver problemas, escribir código limpio y seguir aprendiendo día a día.
+                .
             </p>
             <p>
-                Trabajo principalmente con HTML, CSS y PHP.
-                Para proyectos de mayor envergadura, utilizo frameworks como Yii2.
-                Actualmente estoy desarrollando mi portafolio personal como una forma de mostrar mis proyectos y evolución como desarrollador.
+                Trabajo principalmente con HTML, CSS, PHP, librerías como bootstrap.<br>
+                Para proyectos de mayor envergadura, utilizo frameworks como Yii2.<br>
+                Actualmente estoy desarrollando mi portafolio personal como una forma de mostrar mis proyectos y evolución como desarrollador.<br>
             </p>
             <p>
-                También estoy enfocado en mejorar mi nivel de inglés técnico, especialmente en lo relacionado con errores en PHP y conexiones con bases de datos MySQL.
+                Enfocado en mejorar continua.
             </p>
 
         </section>
