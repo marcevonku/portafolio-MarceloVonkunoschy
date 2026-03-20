08120844d8b48e7b65a8088032039fee1f380e24
Marcelo Vonkunoschy <marcelo.vonku@xnlatam.com>
2026-03-19 22:22:38 -0300

    fotos

--- marcovonkudj/src/Components/Footer.jsx
+++ marcovonkudj/src/Components/Footer.jsx
@@ -1,41 +1,42 @@
 import React from "react";
-import LogoMarco6 from "../img-fondo/logo6.png"
+import Logo8 from "../img-fondo/Logo8.jpg";
 
-function Footer(){
-    return(
+function Footer() {
+    return (
         <div className="row footer">
-            
-                <div className="col">
-                    <img 
-                        src={LogoMarco6} 
-                        alt="Logo de Marco" 
-                        style={{
-                            width: '250px',
-                            float: 'left',
-                        }}
-                    />
-                </div>
-                <div className="col">
-                    <h4 style={{textAlign: 'center',
-                        margin: '0 auto',
-                        textAlignLast: "left",
-                        color: "white",
-                    }}>MarcoVonkuDJ</h4>
-                </div>
-                <div className="col" 
-                    style={{
-                        fontSize: '18px',
-                        color: 'white',
-                    }}>MarTreSA I. T.
-                </div>
 
-        <div className="row">
-        <p style={{
-            textAlign: 'center',
-            color: 'white',
-            
-            }}>Copyright © 2024 MarcoVonkuDJ</p>
-        </div>
+            <div className="col">
+                <img
+                    src={Logo8}
+                    alt="Logo de Marco"
+                    style={{
+                        width: '250px',
+                        float: 'left',
+                    }}
+                />
+            </div>
+            <div className="col">
+                <h4 style={{
+                    textAlign: 'center',
+                    margin: '0 auto',
+                    textAlignLast: "left",
+                    color: "white",
+                }}>MarcoVonkuDJ</h4>
+            </div>
+            <div className="col"
+                style={{
+                    fontSize: '18px',
+                    color: 'white',
+                }}>MarTreSA I. T.
+            </div>
+
+            <div className="row">
+                <p style={{
+                    textAlign: 'center',
+                    color: 'white',
+
+                }}>Copyright © 2024 MarcoVonkuDJ</p>
+            </div>
         </div>
     );
 }
